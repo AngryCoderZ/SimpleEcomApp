a9e7463c6fd923aa42b3c8a323f3dff2a81eee4d
angrycoderz <angrycoderz01@gmail.com>
2023-02-09 22:50:51 +0500

    hashing

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const { v4: uuidv4 } = require('uuid')
+const crypto = require('crypto')
 
 const userSchema = new mongoose.Schema(
     {
@@ -24,19 +26,51 @@ const userSchema = new mongoose.Schema(
                 }
             }
         },
-        password: String,
+        hashed_password: {
+            type: String,
+            required: [true, 'password is required']
+        },
+        salt: String,
     },
     { timestamps: true }
 )
 
 
-userSchema.virtual("hash")
+userSchema.virtual("password")
     .set(function (v) {
-        this.name = v;
+        this._password = v;
+        this.salt = uuidv4();
+        this.hashed_password = this.encryptPassword(v)
     })
     .get(function () {
-        return this.name
+        return this._password
     })
+
+userSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return ""
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        }
+        catch (err) {
+            return "error in encrypting"
+        }
+    }
+}
+
+
+
+
+// userSchema.virtual("hash")
+//     .set(function (v) {
+//         this.name = v;
+//     })
+//     .get(function () {
+//         return this.name
+//     })
 
 
 module.exports = mongoose.model("User", userSchema)
